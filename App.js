3be9e0c86eecae34ad0c49ccc49a1d6230819d5c
Minh Than <hoangminh117@gmail.com>
2022-10-20 12:58:01 -0500

    Added Chart page and packages

--- App.js
+++ App.js
@@ -1,14 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import  Login  from './page/login'
-import Register from './page/register'
+import Login from './page/login';
+import Register from './page/register';
+import Chart from './page/Chart';
+import Activity from './page/Activity';
+import Navbar from './components/Navbar';
+
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       {/* <Login/> */}
+//       <Register />
+//     </View>
+//   );
+// }
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      {/* <Login/> */}
-      <Register/>
-    </View>
+    <>
+      <View style={{ width: '100%', height: '100%' }}>
+        <View style={{ flex: 10 }}>
+          {/* <Activity /> */}
+          <Chart />
+        </View>
+        <View style={{ flex: 1 }}>
+          <Navbar />
+        </View>
+      </View>
+    </>
   );
 }
 
---
+++ page/Chart.js
@@ -0,0 +1,66 @@
+import { StyleSheet, Text, View } from 'react-native';
+import React, { useRef } from 'react';
+import { BarChart } from 'react-native-chart-kit';
+import { Dimensions } from 'react-native';
+
+export default function Chart() {
+  const chartConfig = {
+    backgroundGradientFrom: '#fff',
+    backgroundGradientFromOpacity: 1,
+    backgroundGradientTo: '#fff',
+    backgroundGradientToOpacity: 1,
+    color: () => `rgba(31, 30, 51, 1)`,
+    withInnerLines: false,
+    withVerticalLabels: false,
+  };
+  return (
+    <>
+      <View style={styles.chartWrapper}>
+        <View style={styles.headerWrapper}>
+          <Text style={styles.headerText}>Chart</Text>
+        </View>
+        <View style={styles.chartViewWrapper}>
+          <BarChart
+            data={{
+              labels: ['January', 'February', 'March', 'April', 'May', 'June'],
+              datasets: [
+                {
+                  data: [20, 45, 28, 80, 99, 43],
+                },
+              ],
+            }}
+            width={Dimensions.get('window').width * 0.9}
+            height={Dimensions.get('window').height * 0.6}
+            chartConfig={chartConfig}
+          />
+        </View>
+      </View>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  chartWrapper: {
+    width: '100%',
+    height: '100%',
+    flexDirection: 'column',
+    padding: 25,
+    paddingTop: 45,
+  },
+  backText: {
+    fontSize: 15,
+    flex: 1,
+  },
+  headerText: {
+    fontWeight: 'bold',
+    fontSize: 24,
+    textAlign: 'center',
+  },
+  chartViewWrapper: {
+    width: '100%',
+    height: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingHorizontal: 25,
+  },
+});
