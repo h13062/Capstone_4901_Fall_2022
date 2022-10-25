d8b7331b76c35ea9e5008cce79a862a3aef1f970
NPNG22 <nguyendinhcaodat@gmail.com>
2022-10-25 16:25:34 -0600

    Implement basic screen change and install package

--- App.js
+++ App.js
@@ -7,33 +7,41 @@ import Chart from './page/Chart';
 import Activity from './page/Activity';
 import Navbar from './components/Navbar';
 import BabyProfiles from './page/BabyProfiles';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+export default function App() {
+  const { Navigator, Screen } = createNativeStackNavigator();
+  return (
+    <NavigationContainer>
+      {/* <View style={styles.container}> */}
+      <Navigator>
+        <Screen name="Login" options={{headerShown: false}} component={Login}/>
+        <Screen name="Register" options={{headerShown: false}} component={Register}/>
+        <Screen name="BabyProfiles" component={BabyProfiles}/>
+        {/* <Register /> */}
+        {/* <BabyInfo /> */}
+        </Navigator>
+      {/* </View> */}
+    </NavigationContainer>
+  );
+}
 
 // export default function App() {
 //   return (
-//     <View style={styles.container}>
-//       {/* <Login /> */}
-//       {/* <Register /> */}
-//       <BabyInfo />
-//     </View>
+//     <>
+//       <View style={{ width: '100%', height: '100%' }}>
+//         <View style={{ flex: 10 }}>
+//           <Activity />
+//           {/* <Chart /> */}
+//           {/* <BabyProfiles /> */}
+//         </View>
+//         <View style={{ flex: 1 }}>
+//           <Navbar />
+//         </View>
+//       </View>
+//     </>
 //   );
 // }
-
-export default function App() {
-  return (
-    <>
-      <View style={{ width: '100%', height: '100%' }}>
-        <View style={{ flex: 10 }}>
-          {/* <Activity /> */}
-          <Chart />
-          {/* <BabyProfiles /> */}
-        </View>
-        <View style={{ flex: 1 }}>
-          <Navbar />
-        </View>
-      </View>
-    </>
-  );
-}
 
 const styles = StyleSheet.create({
   container: {
