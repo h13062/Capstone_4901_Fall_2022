800908bbe40249e350903a3af6c7d38b9918167c
Jason Luu <90232287+Jatai1@users.noreply.github.com>
2023-03-09 13:20:39 -0600

    enable jwt to swagger
    provide a button for jwt on swagger

--- BabyTrackerBackEnd/BabyTracker.Api/Program.cs
+++ BabyTrackerBackEnd/BabyTracker.Api/Program.cs
@@ -18,6 +18,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+//add jwt token to swagger
 builder.Services.AddSwaggerGen(c =>
 {
     c.SwaggerDoc("v1", new OpenApiInfo
